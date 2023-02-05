e6dc15781e8ad8ad3a691a60aece87bbf00a17d7
KevinAlexV <KevinAlexV@users.noreply.github.com>
2023-02-05 04:10:36 -0800

    Changes to Level Art

--- MusicalAI/Source/MusicalAI/MusicalAIGameMode.cpp
+++ MusicalAI/Source/MusicalAI/MusicalAIGameMode.cpp
@@ -5,12 +5,63 @@
 #include "UObject/ConstructorHelpers.h"
 #include "Sound/SoundBase.h"
 
+/** Obtain all files in a provided directory, with optional extension filter. All files are returned if Ext is left blank. Returns false if operation could not occur. */
+//UFUNCTION(BlueprintPure, Category = "VictoryBPLibrary|File IO")
+//static bool JoyFileIO_GetFiles(TArray<FString>& Files, FString RootFolderFullPath, FString Ext);
+
+bool JoyFileIO_GetFiles(TArray<FString>& Files, FString RootFolderFullPath, FString Ext)
+{
+	if (RootFolderFullPath.Len() < 1) return false;
+
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Searching files.."));
+	FPaths::NormalizeDirectoryName(RootFolderFullPath);
+
+	IFileManager& FileManager = IFileManager::Get();
+
+	if (Ext == "")
+	{
+		Ext = "*.*";
+	}
+	else
+	{
+		Ext = (Ext.Left(1) == ".") ? "*" + Ext : "*." + Ext;
+	}
+
+	FString FinalPath = RootFolderFullPath + "/" + Ext;
+	FileManager.FindFiles(Files, *FinalPath, true, false);
+	return true;
+}
+
 AMusicalAIGameMode::AMusicalAIGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonSprite"));
+
+	/*if(FPaths::FileExists("F:\\Workspace\\Github\\MusicalAI\\MusicalAI\\Content\\ThirdPerson\\Blueprints"))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("File exists"));
+	}
+	else 
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Does not exist.."));
+	}
+
+	TArray<FString> FileNames;
+
+	/JoyFileIO_GetFiles(FileNames, "F:\\Workspace\\Github\\MusicalAI\\MusicalAI\\Content\\ThirdPerson\\Blueprints", "");
+
+	for (FString name : FileNames) 
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, name);
+	
+	}*/
+
 	if (PlayerPawnBPClass.Class != NULL)
 	{
+		if (GEngine)
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Player Pawn Class is not NULL!"));
+
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
+
